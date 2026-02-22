aa057608c82adcebaae3b1071c192e1767d6d0f9
Isha Choudhary <126189519+Isha2790@users.noreply.github.com>
2026-02-23 02:09:51 +0530

    Time: 0 ms (100%), Space: 42.4 MB (19.19%) - LeetHub

---
+++ 0400-nth-digit/0400-nth-digit.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findNthDigit(int n) {
+        int len = 1;
+        long count = 9;
+        int start = 1;
+        
+        while (n > len * count) {
+            n -= len * count;
+            len++;
+            count *= 10;
+            start *= 10;
+        }    
+        start += (n - 1) / len;
+        String num = Integer.toString(start);
+        return num.charAt((n - 1) % len) - '0';
+    }
+}
